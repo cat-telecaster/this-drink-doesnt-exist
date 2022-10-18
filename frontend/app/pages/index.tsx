3a43cdd4ee9f6496227e63fcaa1471c9b98b483d
kurtis chan <kurtis.chan.1@live.com>
2022-10-18 17:58:10 +0900

    dependencies and index file changes

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Date from '../components/date';
+import InputAdornments from '../components/entry-form-components/text-field';
+import TypeSelectBox from '../components/entry-form-components/type-selectbox';
+import SubmitDeleteButtons from '../components/entry-form-components/buttons';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from "../lib/posts";
@@ -30,11 +33,12 @@ const Home: NextPage<HomeProps> = ({ allPostsData }) => {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>[Your Self Introduction]</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
+            <h2 className={utilStyles.headingLg}>AIAIAIAAIIAIAIAIIIIIIAAAAAA</h2>
+            <div className={utilStyles.entryForm}>
+                <InputAdornments/>
+                <TypeSelectBox/>
+                <SubmitDeleteButtons/>
+            </div>
         </section>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
