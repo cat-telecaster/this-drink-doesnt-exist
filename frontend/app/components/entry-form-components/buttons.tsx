fc60720a8604bf439a3966b289021056f89ab207
kurtis chan <kurtis.chan.1@live.com>
2022-10-19 16:18:54 +0900

    added state interaction for buttons.tsx

--- frontend/app/components/entry-form-components/buttons.tsx
+++ frontend/app/components/entry-form-components/buttons.tsx
@@ -3,14 +3,36 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
+import {useRecoilState, useResetRecoilState} from 'recoil';
+import { inputBoxState, typeSelectorState } from '../states';
 
 export default function SubmitDeleteButtons() {
+    const [inputBoxValues, setInputBoxValues] = useRecoilState(inputBoxState);
+    const [typeSelectorValue, setTypeSelectorValue] = useRecoilState(typeSelectorState);
+
+    const inputBoxReset = useResetRecoilState(inputBoxState);
+    const typeSelectorReset = useResetRecoilState(typeSelectorState);
+
+    const clearAll = () => {
+        inputBoxReset();
+        typeSelectorReset();
+    }
+
     return (
         <Stack direction="row" spacing={2}>
-            <Button variant="outlined" startIcon={<DeleteIcon />}>
+            <Button
+                variant="outlined"
+                onClick={clearAll}
+                startIcon={<DeleteIcon />}
+            >
                 Clear
             </Button>
-            <Button variant="contained" endIcon={<SendIcon />}>
+            <Button
+                variant="contained"
+                disabled={(inputBoxValues.name.err || inputBoxValues.flavour.err || inputBoxValues.size.err || inputBoxValues.price.err
+                    || !typeSelectorValue)}
+                endIcon={<SendIcon />}
+            >
                 Submit
             </Button>
         </Stack>
