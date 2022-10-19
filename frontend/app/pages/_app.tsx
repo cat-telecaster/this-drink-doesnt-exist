4fc1045ae7fe8bd2a421176232e80ce9eaa04dcc
kurtis chan <kurtis.chan.1@live.com>
2022-10-19 16:20:52 +0900

    added RecoilRoot for recoil state manager

--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+  )
 }
 
 export default MyApp
