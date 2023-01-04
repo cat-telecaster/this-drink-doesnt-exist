30e22ee653bda771e9f4ce68d4cb412b60c91af9
kurtis chan <kurtis.chan.1@live.com>
2023-01-04 17:30:15 +0900

    domain.drink for db upload

---
+++ backend/graph/domain/drink.go
@@ -0,0 +1,42 @@
+package domain
+
+import (
+	"encoding/base64"
+)
+
+type Drink struct {
+	Name    string
+	Flavour string
+	Price   int
+	Type    string
+	ML      int
+	Image   []byte
+}
+
+func NewDrink(
+	name string,
+	flavour string,
+	price *int,
+	drinkType string,
+	mL int,
+	imgb64 string,
+) (*Drink, error) {
+	var dPrice = 0
+	if price != nil {
+		dPrice = *price
+	}
+
+	imgBytes, err := base64.StdEncoding.DecodeString(imgb64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Drink{
+		Name:    name,
+		Flavour: flavour,
+		Price:   dPrice,
+		Type:    drinkType,
+		ML:      mL,
+		Image:   imgBytes,
+	}, nil
+}
